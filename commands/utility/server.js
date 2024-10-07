62fbb72d81cd914f8737559a820fa1ce61121220
Nicolas Ravaux <n.ravaux@easi.net>
2024-10-07 13:35:55 +0200

    using eslint rules

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -4,78 +4,75 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '.env.vault' });
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('server')
-        .setDescription('Provides detailed server information.'),
-    async execute(interaction) {
-        const { guild } = interaction;
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Provides detailed server information.'),
+	async execute(interaction) {
+		const { guild } = interaction;
 
-        // Get channel categories
-        const channels = guild.channels.cache;
-        const textChannels = channels.filter(channel => channel.type === 0).size; // GuildText
-        const voiceChannels = channels.filter(channel => channel.type === 2).size; // GuildVoice
-        const categoryChannels = channels.filter(channel => channel.type === 4).size; // GuildCategory
-        const newsChannels = channels.filter(channel => channel.type === 5).size; // GuildAnnouncement
+		// Get boost
+		const boostTier = guild.premiumTier;
 
-        // Get boost
-        const boosts = guild.premiumSubscriptionCount;
-        const boostTier = guild.premiumTier;
+		// Choose color by boost level
+		let embedColor;
+		switch (boostTier) {
+		case 0:
+			// Helha color
+			embedColor = parseInt(process.env.helhaColor, 16);
+			break;
+		case 1:
+			// Or
+			embedColor = 0xFFD700;
+			break;
+		case 2:
+			// Silver
+			embedColor = 0xC0C0C0;
+			break;
+		case 3:
+			// Bronze
+			embedColor = 0xCD7F32;
+			break;
+		default:
+			// Helha color
+			embedColor = parseInt(process.env.helhaColor, 16);
+		}
 
-        // Choose color by boost level
-        let embedColor;
-        switch (boostTier) {
-            case 0:
-                embedColor = parseInt(process.env.helhaColor, 16); // Helha color
-                break;
-            case 1:
-                embedColor = 0xFFD700; // Or
-                break;
-            case 2:
-                embedColor = 0xC0C0C0; // Silver
-                break;
-            case 3:
-                embedColor = 0xCD7F32; // Bronze
-                break;
-            default:
-                embedColor = parseInt(process.env.helhaColor, 16); // Helha color
-        }
+		// Embded creation
+		const serverInfoEmbed = new EmbedBuilder()
+			.setTitle('📊 Server & bot informations')
+			.setThumbnail(interaction.guild.iconURL({ dynamic: true, size: 1024 }))
+			.setColor(embedColor)
+			.setTimestamp()
+			.setFooter({
+				text: `Server ID : ${guild.id}`,
+				iconURL: guild.iconURL({ dynamic: true }),
+			});
 
-        // Embded creation
-        const serverInfoEmbed = new EmbedBuilder()
-            .setTitle(`📊 Server & bot informations`)
-            .setThumbnail(interaction.guild.iconURL({ dynamic: true, size: 1024 }))
-            .setColor(embedColor)
-            .setTimestamp()
-            .setFooter({ 
-                text: `Server ID : ${guild.id}`, 
-                iconURL: guild.iconURL({ dynamic: true }) 
-            });
-
-        // Add banner is exist
-        if (guild.banner) {
-            serverInfoEmbed.setImage(guild.bannerURL({ dynamic: true, size: 1024 }));
-        }
+		// Add banner is exist
+		if (guild.banner) {
+			serverInfoEmbed.setImage(guild.bannerURL({ dynamic: true, size: 1024 }));
+		}
 
 		// Get version
 		const packageJson = require('../../package.json');
 		const botVersion = packageJson.version;
 
-        // Add fields
-        serverInfoEmbed.addFields(
+		// Add fields
+		serverInfoEmbed.addFields(
 			{ name: '🔍 **__Server informations__**', value: ' ', inline: false	},
-			{ name: '💬 Name', value:  guild.name, inline: true},
-            { name: '👥 Membres', value: `${guild.memberCount.toLocaleString()}`, inline: true},
-            { name: '🏆 Boost level', value: `Tier ${boostTier}`, inline: true},         
-            { name: '📅 Creation date', value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>`, inline: true},
+			{ name: '💬 Name', value:  guild.name, inline: true },
+			{ name: '👥 Membres', value: `${guild.memberCount.toLocaleString()}`, inline: true },
+			{ name: '🏆 Boost level', value: `Tier ${boostTier}`, inline: true },
+			{ name: '📅 Creation date', value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>`, inline: true },
 			{ name: '\u200B', value: '\u200B' },
-			{ name: '🔍 **__Bot informatons__**', value: ' ', inline: false},
-			{ name: '**Version**', value: `${botVersion}`, inline: true},
-			{ name: '**Support**', value: `@nicovoltes`, inline: true},
-			{ name: '**Documentation**', value: `[Documentation](https://github.com/NicolasR64/HELHa-Bot)`, inline: true},
+			{ name: '🔍 **__Bot informatons__**', value: ' ', inline: false },
+			{ name: '**Version**', value: `${botVersion}`, inline: true },
+			{ name: '**Support**', value: '@nicovoltes', inline: true },
+			{ name: '**Documentation**', value: '[Documentation](https://github.com/NicolasR64/HELHa-Bot)', inline: true },
 			{ name: '🔗 Link', value: `[Visit link](${guild.vanityURLCode ? `https://discord.gg/${guild.vanityURLCode}` : 'https://discord.gg/'})`, inline: true },
-			{ name: '**Developer**', value: `Ravaux Nicolas(@nicovoltes)`, inline: true}
-        );
+			{ name: '**Developer**', value: 'Ravaux Nicolas(@nicovoltes)', inline: true },
+		);
 
-        await interaction.reply({ embeds: [serverInfoEmbed] });
-    },
+		await interaction.reply({ embeds: [serverInfoEmbed] });
+	},
 };
--- commands/utility/user.js
+++ commands/utility/user.js
@@ -4,37 +4,37 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '.env.vault' });
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('user')
-        .setDescription('Provides information about a user.')
-        .addUserOption(option => 
-            option.setName('target')
-                .setDescription('Select a user to get information about.')),
-    async execute(interaction) {
-        // Get user data
-        const targetUser = interaction.options.getUser('target') || interaction.user;
-        const member = interaction.guild.members.cache.get(targetUser.id);
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Provides information about a user.')
+		.addUserOption(option =>
+			option.setName('target')
+				.setDescription('Select a user to get information about.')),
+	async execute(interaction) {
+		// Get user data
+		const targetUser = interaction.options.getUser('target') || interaction.user;
+		const member = interaction.guild.members.cache.get(targetUser.id);
 
-        // Create embed
-        const userInfoEmbed = new EmbedBuilder()
-            .setColor(parseInt(process.env.helhaColor, 16))
-            .setTitle(`🔍 Informations sur l'utilisateur ${targetUser.username}`)
-            .setThumbnail(targetUser.displayAvatarURL({ dynamic: true, size: 1024 })) 
-            .setTimestamp()
-            .setFooter({ text: 'Thank you for being part of our community !' });
+		// Create embed
+		const userInfoEmbed = new EmbedBuilder()
+			.setColor(parseInt(process.env.helhaColor, 16))
+			.setTitle(`🔍 Informations sur l'utilisateur ${targetUser.username}`)
+			.setThumbnail(targetUser.displayAvatarURL({ dynamic: true, size: 1024 }))
+			.setTimestamp()
+			.setFooter({ text: 'Thank you for being part of our community !' });
 
-        // Add fields
-        userInfoEmbed.addFields(
-            { name: '👤 User name', value: targetUser.tag, inline: true },
-            { name: '🆔 Identifiant', value: targetUser.id, inline: true },
-            { name: '📅 Joins the', value: `${member.joinedAt.toDateString()}`, inline: true },
-            { name: '🎉 Acc. created on', value: `${targetUser.createdAt.toDateString()}`, inline: true },
-            { name: '💬 Status', value: `${member.presence?.status || 'X'}`, inline: true },
-            { name: '🔖 Roles', value: member.roles.cache.map(role => role.name).join(', ') || 'No role', inline: true },
-            { name: '🖼️ Avatar', value: `[View here](${targetUser.displayAvatarURL()})`, inline: true },
-            { name: '🔗 Mention', value: `<@${targetUser.id}>`, inline: true }
-        );
+		// Add fields
+		userInfoEmbed.addFields(
+			{ name: '👤 User name', value: targetUser.tag, inline: true },
+			{ name: '🆔 Identifiant', value: targetUser.id, inline: true },
+			{ name: '📅 Joins the', value: `${member.joinedAt.toDateString()}`, inline: true },
+			{ name: '🎉 Acc. created on', value: `${targetUser.createdAt.toDateString()}`, inline: true },
+			{ name: '💬 Status', value: `${member.presence?.status || 'X'}`, inline: true },
+			{ name: '🔖 Roles', value: member.roles.cache.map(role => role.name).join(', ') || 'No role', inline: true },
+			{ name: '🖼️ Avatar', value: `[View here](${targetUser.displayAvatarURL()})`, inline: true },
+			{ name: '🔗 Mention', value: `<@${targetUser.id}>`, inline: true },
+		);
 
-        await interaction.reply({ embeds: [userInfoEmbed] });
-    },
+		await interaction.reply({ embeds: [userInfoEmbed] });
+	},
 };
